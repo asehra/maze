1b0dcd0a94027fcd5e916032269f4ffa67f3ab92
Ashish Sehra <asehra@gmail.com>
2015-10-04 13:07:58 +0100

    Maze generation

---
+++ app/models/maze.rb
@@ -0,0 +1,62 @@
+class Maze
+  attr_reader :width, :height, :entry, :exit, :structure
+
+  def initialize(width:, height:)
+    @width = width
+    @height = height
+    @entry = { row: 1, col: 0 }
+    @exit = { row: 2*height - 1, col: 2 * width }
+    @structure = generate
+  end
+
+  private
+
+  def generate
+    grid = grid_with_rooms
+    grid[entry[:row]][entry[:col]] = 0
+    grid[exit[:row]][exit[:col]] = 0
+
+    maze = [[entry[:row], entry[:col] + 1]]
+    pending = neighbours_of(maze[0]) - maze
+
+    while pending.length > 0 do
+      node = pending.delete_at(rand(pending.length))
+      neighbours = neighbours_of(node)
+      join_point = (neighbours & maze).sample
+
+      maze << connect(node, grid, at: join_point)
+      pending = pending | (neighbours - maze)
+    end
+    grid
+  end
+
+  def connect(node, grid, at:)
+    wall = [(node[0] + at[0])/2, (node[1] + at[1])/2]
+    grid[wall[0]][wall[1]] = 0
+    node
+  end
+
+  def neighbours_of(ref)
+    directions = [[-2, 0], [2, 0], [0, -2], [0, 2]]
+    neighbours = directions.map { |d| [ref[0] + d[0], ref[1] + d[1]] }
+    neighbours.select do |room|
+      (0..(2*height)).include?(room[0]) &&
+        (0..(2*width)).include?(room[1])
+    end
+  end
+
+  def grid_with_rooms
+    (grid = empty_grid).each_with_index do |row, y|
+      next if y.even?
+      row.each_with_index do |value, x|
+        next if x.even?
+        grid[y][x] = 0
+      end
+    end
+  end
+
+  def empty_grid
+    row = [1] * (2*width + 1)
+    [].tap { |g| (2*height + 1).times { g << row.dup } }
+  end
+end
---
+++ spec/models/maze_spec.rb
@@ -0,0 +1,53 @@
+RSpec.describe do
+  describe Maze do
+    let(:width) { 7 }
+    let(:height) { 5 }
+
+    subject(:maze) { Maze.new(width: width, height: height) }
+
+    describe '#initialize' do
+      its(:width) { should eq(width) }
+      its(:height) { should eq(height) }
+
+      it 'sets entry in first column on valid odd numbered row next to a room' do
+        expect(maze.entry[:row]).to be_odd
+        expect(maze.entry[:row]).to be_between(0, maze.height * 2 - 1)
+        expect(maze.entry[:col]).to eq(0)
+      end
+
+      it 'sets exit in last column on valid odd numbered row next to a room' do
+        expect(maze.exit[:row]).to be_odd
+        expect(maze.exit[:row]).to be_between(0, maze.height * 2 - 1)
+        expect(maze.exit[:col]).to eq(2 * maze.width)
+      end
+    end
+
+    describe '#structure' do
+      it 'is a 2D grid of dimensions (2*width + 1) x (2*height + 1)' do
+        expect(maze.structure.length).to eq(2*maze.height + 1)
+        maze.structure.each do |row|
+          expect(row.length).to eq(2*maze.width + 1)
+        end
+      end
+
+      it 'has height*width "rooms" denoted by 0s at every [odd,odd] coordinate' do
+        maze.structure.each_with_index do |row, y|
+          next if y.even?
+          row.each_with_index do |value, x|
+            next if x.even?
+            expect(value).to eq(0)
+          end
+        end
+      end
+
+      it 'should show the entry and exit in the structure' do
+        expect(maze.structure[maze.entry[:row]][maze.entry[:col]]).to eq(0)
+        expect(maze.structure[maze.exit[:row]][maze.exit[:col]]).to eq(0)
+      end
+    end
+
+    context 'validity' do
+      it { should have_a_valid_solution }
+    end
+  end
+end
