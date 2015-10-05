d3e6478520217dfc9ba522bc46d2165cec2777d0
Ashish Sehra <asehra@gmail.com>
2015-10-05 12:57:17 +0100

    Render maze using JS. Refactored solver.

--- app/assets/javascripts/MazeSolver.js
+++ app/assets/javascripts/MazeSolver.js
@@ -1,18 +1,21 @@
 (function(window) {
-  var tree = []
-  var Node = function(location, parent) {
-    this.parent = parent;
-    this.location = location;
-    tree.push(this);
-  }
-
-  Node.prototype.isParent = function(location) {
-    return this.parent && arrayEquals(this.parent.location, location);
-  }
-
   var arrayEquals = function(arr1, arr2) {
     return JSON.stringify(arr1) === JSON.stringify(arr2);
   };
+
+  var Node = function(location, parent) {
+    this.parent = parent;
+    this.location = location;
+  }
+
+  Node.prototype.isDescendantOf = function(location) {
+    return this.parent && arrayEquals(this.parent.location, location);
+  }
+
+  Node.prototype.neighbouringLocation = function(direction) {
+    return [this.location[0] + direction[0],
+        this.location[1] + direction[1]];
+  }
 
   var directions = [[-1, 0], [0, 1], [1, 0], [0, -1]];
   var neighbourNode, output;
@@ -21,16 +24,12 @@
     if(arrayEquals(start.location, end)) return start;
     var i = 0;
     for(i=0; i<directions.length; i++) {
-      var neighbour = [start.location[0] + directions[i][0],
-        start.location[1] + directions[i][1]];
-      if(!start.isParent(neighbour)) {
-        if(maze[neighbour[0]][neighbour[1]] === 0) {
-          neighbourNode = new Node(neighbour, start);
-          output = locate(maze, neighbourNode, end);
-          if(output !== -1) {
-            return output;
-          }
-        }
+      var neighbour = start.neighbouringLocation(directions[i]);
+      if(start.isDescendantOf(neighbour)) { continue; }
+
+      if(maze[neighbour[0]][neighbour[1]] === 0) {
+        output = locate(maze, new Node(neighbour, start), end);
+        if(output !== -1) { return output; }
       }
     }
 
@@ -47,24 +46,24 @@
 
   var MazeSolver = {
     solve: function(maze) {
-      var i, j, row,
-      root = new Node([1,0]),
+      var root = new Node([1,0]),
       end = [maze.length - 2, maze[0].length - 1];
 
       var endNode = locate(maze, root, end);
 
       plotPath(maze, endNode);
-      var output = [];
-      for(i = 0; i<maze.length; i++) {
-        row = maze[i];
+      return this.print(maze);
+    },
 
-        output[i] = row.join('').replace(/0/g, ' ').replace(/1/g, '▓');
+    print: function(maze) {
+      var i, output = [];
+      for(i = 0; i<maze.length; i++) {
+        output[i] = maze[i].join('').replace(/0/g, ' ').replace(/1/g, '▓');
       }
 
       return output.join('\n');
     }
   }
-
 
   window.MazeSolver = MazeSolver;
 })(window)
